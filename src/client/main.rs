5b2654f4f7a967c361cdbec87c3bdf46826b2a5f
56419249 <clayshowtv@gmail.com>
2022-04-01 09:02:24 +0300

    ima turn into tauri app less goo

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+  )]
+
 pub mod scheduler;
 pub mod task;
 pub mod mappings;
@@ -10,13 +15,18 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 const ZOOM_ATTENDER_PATH: &str = "\\ZoomAttender";
 
 
-fn main() -> Result<()> {
-    println!("this is client sir");
-    let resp = std::fs::read_to_string("resp.json").unwrap();
-    let des: Tasks = serde_json::from_str(&resp).unwrap();
-    println!("{:?}", des);
-    des.make()?;
-    Ok(())
+fn main() {
+
+    tauri::Builder::default()
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+
+    //println!("this is client sir");
+    //let resp = std::fs::read_to_string("resp.json").unwrap();
+    //let des: Tasks = serde_json::from_str(&resp).unwrap();
+    //println!("{:?}", des);
+    //des.make()?;
+    //Ok(())
     //let _ = std::process::Command::new("cmd.exe")
     //            .arg("/c")
     //            .arg("pause")
